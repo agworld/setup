e5a7d2133e3d9e83e24ffd40f1e62007d553fb23
Stephen Wrathall <stephen.wrathall@agworld.com.au>
2018-05-11 11:53:41 -0600

    Make clear that this script doesn't work automatically yet
    See TODO comment about current issues.

--- extra_default_user_setup.sh
+++ extra_default_user_setup.sh
@@ -1,10 +1,10 @@
+# TODO: Get this script working automatically.
+#       Currently it fails for a few reasons. RVM due to GPG signature verification. POW due to permissions.
+echo "ERROR: This script doesn't work automatically yet!!! Run it manually for now on the laptop you're setting up."
+exit 1
+
 # Create a default workspace
 mkdir ~/workspace
-
-# Install rvm (c.f: https://rvm.io/):
-# c.f: https://stackoverflow.com/questions/15691977/why-start-a-shell-command-with-a-backslash
-curl -sSL https://get.rvm.io | bash -s stable
-rvm install 2.3.1
 
 # Install Pow (c.f: http://pow.cx/):
 curl get.pow.cx | sh
@@ -13,3 +13,8 @@ ln -s ~/workspace/docsplit-server
 ln -s ~/workspace/website
 ln -s ~/workspace/website my.agworld
 ln -s ~/workspace/reporting-service
+
+# Install rvm (c.f: https://rvm.io/):
+# c.f: https://stackoverflow.com/questions/15691977/why-start-a-shell-command-with-a-backslash
+curl -sSL https://get.rvm.io | bash -s stable
+rvm install 2.3.1
